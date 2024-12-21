d417f6e8e1b5bf58da342c830e3420e3c36c8579
sgandla <sgandla@riptgames.com>
2024-12-21 12:10:24 -0800

    html projects

---
+++ finalproject/app.js
@@ -0,0 +1,16 @@
+
+
+
+
+function navSlide()
+{
+    const nav = document.querySelector(".navbar");
+    const menu = document.querySelector(".nav-links");
+    const burger= document.querySelector(".burger");
+
+    burger.addEventListener("click",()=>{
+        menu.classList.toggle("nav-active");
+    burger.classList.toggle("toggle");
+    })
+}
+navSlide();
